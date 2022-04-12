36d46902319bd145d558eaac099933969ea61a40
wilnowora <wilnooworaa@gmail.com>
2022-04-12 20:42:57 +0800

    ganti background color

--- script.js
+++ script.js
@@ -5,5 +5,15 @@ document.onmousemove = (event) => {
     var y = event.clientY;
     box.style.left = x - (box.clientWidth/2) + "px";
     box.style.top = y - (box.clientHeight/2) + "px";
-    
+
+}
+
+function random(number) {
+    return Math.floor(Math.random()* number)
+}
+
+document.onclick = function () {
+acak = "rgb(" + random(255) +"," + random(255) +"," +random(255) + ")";
+kotak123.style.background = acak
+
 }
