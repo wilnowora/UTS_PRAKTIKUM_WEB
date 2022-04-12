aab570da1bb3dd61214151131d15e7ec066c5b67
wilnowora <wilnooworaa@gmail.com>
2022-04-12 19:39:55 +0800

    box bergerak mengikuti mouse

--- script.js
+++ script.js
@@ -1,19 +1,9 @@
-let kotak123 = document.getElementById('kotak123');
+let box = document.getElementById('box');
 
 document.onmousemove = (event) => {
     var x = event.clientX;
     var y = event.clientY;
-    kotak123.style.left = x - (kotak123.clientWidth/2) + "px";
-    kotak123.style.top = y - (kotak123.clientWidth/2) + "px";
-
-}
-
-function random(number) {
-    return Math.floor(Math.random()* number)
-}
-
-document.onclick = function () {
-acak = "rgb(" + random(255) +"," + random(255) +"," +random(255) + ")";
-kotak123.style.background = acak
-
+    box.style.left = x - (box.clientWidth/2) + "px";
+    box.style.top = y - (box.clientHeight/2) + "px";
+    
 }
